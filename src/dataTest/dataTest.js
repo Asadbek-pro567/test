aef009f4f3fd0d591a8ab61a3738569e5ec9b09b
Asadbek-pro567 <85244820+Asadbek-pro567@users.noreply.github.com>
2023-01-27 22:00:38 +0500

    lkjhgfd

--- src/dataTest/dataTest.js
+++ src/dataTest/dataTest.js
@@ -1,22 +1,26 @@
 export const tests = [
     {
         id:1,
-        savol: "2+2 necciga teng",
+        savol: "1) 2+2 necciga teng",
         javob: 4,
         otvetlar:[
             {   
+                userId: 'a',
                 testId: 1,
                 otvet: 4 
             },
             {
+                userId: 'b',
                 testId: 2,
                 otvet: 1 
             },
             ,{
+                userId: 'c',
                 testId: 3,
                 otvet: 2 
             }
             ,{
+                userId: 'd',
                 testId: 4,
                 otvet: 3 
             }
@@ -24,22 +28,26 @@ export const tests = [
     },
     {
         id:2,
-        savol: "3+3 necciga teng",
+        savol: "2) 3+3 necciga teng",
         javob: 6,
         otvetlar:[
             {   
+                userId: 'a',
                 testId: 5,
                 otvet: 4 
             },
             {
+                userId: 'b',
                 testId: 6,
                 otvet: 6
             },
             ,{
+                userId: 'c',
                 testId: 7,
                 otvet: 2 
             }
             ,{
+                userId: 'd',
                 testId: 8,
                 otvet: 3 
             }
@@ -47,22 +55,26 @@ export const tests = [
     },
     {
         id:3,
-        savol: "5+5 necciga teng",
+        savol: "3) 5+5 necciga teng",
         javob: 10,
         otvetlar:[
             {   
+                userId: 'a',
                 testId: 9,
                 otvet: 4 
             },
             {
+                userId: 'b',
                 testId: 10,
                 otvet: 6
             },
             ,{
+                userId: 'c',
                 testId: 11,
                 otvet: 10 
             }
             ,{
+                userId: 'd',
                 testId: 12,
                 otvet: 3 
             }
@@ -70,22 +82,26 @@ export const tests = [
     },
     {
         id:4,
-        savol: "6+6 necciga teng",
+        savol: "4) 6+6 necciga teng",
         javob: 12,
         otvetlar:[
             {   
+                userId: 'a',
                 testId: 13,
                 otvet: 12 
             },
             {
+                userId: 'b',
                 testId: 14,
                 otvet: 6
             },
             ,{
+                userId: 'c',
                 testId: 15,
                 otvet: 14 
             }
             ,{
+                userId: 'd',
                 testId: 16,
                 otvet: 11 
             }
@@ -93,22 +109,26 @@ export const tests = [
     },
     {
         id:5,
-        savol: "5+3 necciga teng",
+        savol: "5) 5+3 necciga teng",
         javob: 8,
         otvetlar:[
             {   
+                userId: 'a',
                 testId: 17,
                 otvet: 4 
             },
             {
+                userId: 'b',
                 testId: 18,
                 otvet: 6
             },
             ,{
+                userId: 'c',
                 testId: 19,
                 otvet: 8 
             }
             ,{
+                userId: 'd',
                 testId: 20,
                 otvet: 9 
             }
--- src/pages/Test/Tests.jsx
+++ src/pages/Test/Tests.jsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { tests } from '../../dataTest/dataTest'
+import './Tests.scss'
 
+const itemId = []
 function Tests() {
     const otvet = []
+    
     const [count, setCount] = useState(0)
+    const [bgColor, setBgColor] = useState(0)
+    const [colorrr, setColor] = useState(0)
+
 
     const done = (e)=>{
         e.preventDefault()
         tests?.map((e)=>{
-            const namee = document.getElementsByName(e.id)
+            const namee = document.getElementsByTagName('input')
             for (let i = 0; i < namee.length; i++){
+                console.log(namee.id);
                 if(namee[i].checked){
                     const val = namee[i].value
+                    setBgColor(val)
                     if(val == e.javob){
                         otvet.push(val)
+                        console.log(otvet);
                         for(let o = 1; o <= otvet.length; o++){
                             setCount(o)
                         }
@@ -23,9 +32,32 @@ function Tests() {
         })
     }
 
+    const pusher = (e)=>{
+        itemId.push(e)
+        console.log(e.target);
+    }
+    
+    useEffect(()=>{
+        console.log();
+    }, [colorrr])
+
+    function checkId(chek) {
+        return chek == 2
+    }
+
   return (
-    <div>
-        <h1>Test</h1>
+    <div className='container'>
+        <h1>Matem</h1>
+        <p>2 : 40 : 50</p>
+        <ul>
+            <h5>Matem (3.1ball)</h5>
+            <h6 id='demo'></h6>
+            {
+                tests?.map((e,i)=>(
+                    <li className={pusher? 'active': ''}>{e.id}</li>
+                ))
+            }
+        </ul>
         <ol>
             <form action="#" onSubmit={done}>
             {
@@ -36,7 +68,12 @@ function Tests() {
                             e.otvetlar.map((item)=>(
                                 <>
                                 <input type="radio" id={item.testId} name={e.id} value={item.otvet}/>
-                                <label htmlFor={item.testId}>{item.otvet}</label>
+                                <label onClick={()=>{
+                                    if(!itemId.includes(e.id))
+                                    itemId.push(e.id)
+                                    setColor(itemId.find((i)=> i == e.id ))
+                                    setBgColor(item.testId)
+                                    }} className={bgColor == item.testId? 'label__active' : ''} htmlFor={item.testId}>{item.userId}) {item.otvet}</label>
                                 </>
                             ))    
                         }</h4>
@@ -46,7 +83,7 @@ function Tests() {
             <button type='submit'>Done!</button>
             </form>
         </ol>
-        <h2>Test yakunlandi togri javoblar soni: {count}</h2>
+        <h2>Test yakunlandi togri javoblar soni: {count * 100 / 5}%</h2>
     </div>
   )
 }
